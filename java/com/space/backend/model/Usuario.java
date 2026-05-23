38dc7a7f95dc87a102aac104813bb1f4c76def3f
Murilo Fink <mramosfink@gmail.com>
2026-05-23 00:02:27 -0300

    Add files via upload

---
+++ java/com/space/backend/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.space.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String senha;
+
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+}
+
