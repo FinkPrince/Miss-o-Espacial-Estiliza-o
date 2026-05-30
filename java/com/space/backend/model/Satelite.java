2b9745ed29a3a99a1539fbf6a35f9ca61135a221
Murilo Fink <mramosfink@gmail.com>
2026-05-30 19:47:58 -0300

    Update Satelite.java

--- java/com/space/backend/model/Satelite.java
+++ java/com/space/backend/model/Satelite.java
@@ -10,27 +10,20 @@ public class Satelite {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
-
     @NotNull(message = "Massa é obrigatória")
     @Min(0)
     private Double massa;
-
     @NotNull(message = "Energia é obrigatória")
     @Min(0) @Max(100)
     private Double energia;
-
+    
     @NotBlank
     private String orbita;       // LEO | GEO | LUNAR
-
     private String funcao;       // Espionagem | Localização / GPS | Comunicação
-
     private String tempoOrbita;  // 7 meses | 1 ano e 4 meses | 2 anos
-
     private String status = "Inativo";
-
     public Satelite() {}
 
     public Satelite(String nome, Double massa, Double energia,
@@ -46,25 +39,18 @@ public class Satelite {
 
     public Long   getId()                    { return id; }
     public void   setId(Long id)             { this.id = id; }
-
     public String getNome()                  { return nome; }
     public void   setNome(String nome)       { this.nome = nome; }
-
     public Double getMassa()                 { return massa; }
     public void   setMassa(Double massa)     { this.massa = massa; }
-
     public Double getEnergia()               { return energia; }
     public void   setEnergia(Double energia) { this.energia = energia; }
-
     public String getOrbita()                { return orbita; }
     public void   setOrbita(String orbita)   { this.orbita = orbita; }
-
     public String getFuncao()                { return funcao; }
     public void   setFuncao(String funcao)   { this.funcao = funcao; }
-
     public String getTempoOrbita()                   { return tempoOrbita; }
     public void   setTempoOrbita(String tempoOrbita) { this.tempoOrbita = tempoOrbita; }
-
     public String getStatus()                { return status; }
     public void   setStatus(String status)   { this.status = status; }
 }
